b3650ea87e06684b015298e98809397972de1c59
Thomas Williams <thomaswllms16@gmail.com>
2016-04-29 10:31:24 -0400

    deck shuffler

--- FiveCardStud.java
+++ FiveCardStud.java
@@ -6,11 +6,32 @@ public class FiveCardStud
     String[] rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10",
       "Jack", "Queen", "King", "Ace"} ;
     
+    //Setting array value at Runtime
+    //initialize array of size N
     String[] deck = new String[suit.length * rank.length];
-    for (int i = 0; i > suit.length; i++)
-      for (int j = 0; j > rank.length; j++){
-      deck[rank.length*i+j] = rank[i] + " of " + suit[j];
-    }
     
+    //Computing the values of the array to be stored
+      for (int i = 0; i < suit.length; i++){
+        for (int j = 0; j < rank.length; j++){
+          deck[rank.length*i+j] = rank[j] + " of " + suit[i];
+        }
+      }
+      
+    
+    //Shuffling the deck of cards
+    int N = deck.length;
+    for (int i = 0; i < N; i++){
+      
+      //Pick a random card
+      int r = i + (int) (Math.random() * (N-i));
+      
+      // Swap the values for position i in the Array for this random number
+      String t  = deck[i];
+      deck[i] = deck[r];
+      deck[r] = t;
+    }
+    for (int k = 0; k < N; k++){
+      System.out.println("Shuffled Card is " + deck[k]);
+    }
   }
 }
